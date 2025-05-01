47460e558a956d870bc0e807cfd48297827c5a39
João Teixeira <joao.teixeira@ifro.edu.br>
2025-05-01 01:22:04 +0000

    configuracao de template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <RouterLink to="/">Home</RouterLink>
-  <RouterLink to="/about">About</RouterLink>
+  <SideNavbar />
 
-  <RouterView />
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <NavBar />
+
+    <div class="container-fluid py-2">
+      <RouterView />
+
+    </div>
+
+  </main>
+
+  <SettingsPlugin />
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import SideNavbar from './components/SideNavbar.vue';
+import NavBar from './components/NavBar.vue';
+import SettingsPlugin from './components/SettingsPlugin.vue';
 </script>
-
-
